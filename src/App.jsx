c4d0e824465d17fa10abf68ca35c1b129f55b5da
Ayoub Atiqi <ayoubatiqi123@gmail.com>
2023-12-08 13:56:21 +0100

    logic of to do list

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import { useRef, useState } from 'react';
 
 function App() {
+  const [todos,setTodos] = useState([])
+
+  const inputRef = useRef()
+
+  const handleAddToDo = () => {
+    const text = inputRef.current.value;
+    const newItem = {completed:false,text}
+    setTodos([...todos,newItem])
+    inputRef.current.value="";
+  }
+
+  const handleItemDone = (index) => {
+    const newTodos = [...todos]
+    newTodos[index].completed = !newTodos[index].completed
+    setTodos(newTodos)
+  }
+
+  const handleDeleteItem = (index) => {
+    const newTodos = [...todos];
+    newTodos.splice(index,1)
+    setTodos(newTodos)
+  }
+
   return (
     <div className="App">
+      <h2>To Do List</h2>
+      <div className='to-do-container'>
+        <ul>
+          {todos.map(({text,completed},index) => {
+            return (
+                <div className='item'>
+                   <li
+                  className={ completed ? "done" : ""}
+                  key={index}
+                    onClick={()=>handleItemDone(index)}
+                  > 
+                    {text}
+                  </li>
+                  <span onClick={()=> handleDeleteItem (index)}>X</span>
+                </div> 
+            );
+          })}
+        </ul>
+        <input ref={inputRef} placeholder='Enter item' />
+        <button onClick={handleAddToDo}>Add</button>
+      </div>
     </div>
   );
 }
